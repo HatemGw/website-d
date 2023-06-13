8e4e4d6c695549f997159e1e75305a0d2ee04375
HatemGw <aassdd127zzee@gmail.com>
2023-06-13 17:12:59 +0200

    t

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@
 
 
 //main varibales
+let connectionValue = document.querySelector(".warehouse-data .connection .value span");
 let mq2Value = document.querySelector(".warehouse-data .mq-2 .value span");
 let dht22Value = document.querySelector(".warehouse-data .dht22 .value span");
 let timeValue = document.querySelector(".warehouse-data .time .value span");
 
+
+var conncetionMQ2;
+var connecctionDHT22;
 let i = 0;
 let date = new Date();
 function timeout() {
   setTimeout(function () {
     getMq2();
     getDht22();
+    getConnection();
     let date = new Date();
     timeValue.innerHTML= `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
@@ -31,6 +36,9 @@ function getMq2() {
       //create container for data
       //sensor info
       mq2Value.innerHTML = data;
+      if(data == 0){
+        conncetionMQ2 = 0;
+      }
       //time info
 
 
@@ -51,6 +59,9 @@ function getDht22() {
       //create container for data
       //sensor info
       dht22Value.innerHTML = data;
+      if(data == 0){
+        connecctionDHT22 = 0;
+      }
       //time info
 
 
@@ -61,6 +72,16 @@ function getDht22() {
     });
   });
 
+}
+
+//connection
+function getConnection(){
+  if((conncetionMQ2 ==0)&& (connecctionDHT22 ==0)){
+    connectionValue.innerHTML = 'offline';
+  }
+  else{
+    connectionValue.innerHTML ='online';
+  }
 }
 
 function getWeather(){
