1bf6e03a1454502bf5945904661eec0860dc0b94
Hatem Gw <78381038+HatemGw@users.noreply.github.com>
2023-06-16 03:03:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+//indexed database
+const dbName = 'warehouse';
+const dbVersion = 1;
+
+const request = indexedDB.open(dbName, dbVersion);
+
+request.onerror = (event) => {
+  console.log('Database error:', event.target.errorCode);
+};
+
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  const objectStore = db.createObjectStore('warehouseData', { keyPath: 'id', autoIncrement: true });
+  objectStore.createIndex('time', 'timeValue');
+  objectStore.createIndex('mq2', 'mq2Value');
+  objectStore.createIndex('dht22', 'dht22Value');
+  objectStore.createIndex('connection', 'connectionValue');
+};
+
+
 //main variables
 let connectionValue = document.querySelector(".warehouse-data .connection .value span");
 let mq2Value = document.querySelector(".warehouse-data .mq-2 .value span");
@@ -17,6 +37,31 @@ function timeout() {
     timeout();
   }, 1000);
 }
+function storeData(timeValue, mq2Value, dht22Value, connectionValue) {
+  const request = indexedDB.open(dbName, dbVersion);
+
+  request.onerror = (event) => {
+    console.log('Database error:', event.target.errorCode);
+  };
+
+  request.onsuccess = (event) => {
+    const db = event.target.result;
+    const transaction = db.transaction(['warehouseData'], 'readwrite');
+    const objectStore = transaction.objectStore('warehouseData');
+    const request = objectStore.add({
+      timeValue: timeValue,
+      mq2Value: mq2Value,
+      dht22Value: dht22Value,
+      connectionValue: connectionValue
+    });
+    request.onerror = (event) => {
+      console.log('Data store error:', event.target.errorCode);
+    };
+    transaction.oncomplete = () => {
+      console.log('Data stored successfully.');
+    };
+  };
+}
 
 function getMq2() {
   fetch(
@@ -27,6 +72,9 @@ function getMq2() {
       if (data == 0) {
         connectionMQ2 = 0;
       }
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      storeData(time, data, null, null);
     });
   });
 }
@@ -40,6 +88,9 @@ function getDht22() {
       if (data == 0) {
         connectionDHT22 = 0;
       }
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      storeData(time, null, data, null);
     });
   });
 }
@@ -47,9 +98,52 @@ function getDht22() {
 function getConnection() {
   if (connectionMQ2 == 0 && connectionDHT22 == 0) {
     connectionValue.innerHTML = "offline";
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    storeData(time, null, null, "offline");
   } else {
     connectionValue.innerHTML = "online";
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    storeData(time, null, null, "online");
   }
 }
+function displayData() {
+  const request = indexedDB.open(dbName, dbVersion);
+
+  request.onerror = (event) => {
+    console.log('Database error:', event.target.errorCode);
+  };
+
+  request.onsuccess = (event) => {
+    const db = event.target.result;
+    const transaction = db.transaction(['warehouseData'], 'readonly');
+    const objectStore = transaction.objectStore('warehouseData');
+    const getAllRequest = objectStore.getAll();
+    getAllRequest.onsuccess = (event) => {
+      const data = event.target.result;
+      console.log(data);
+      // Display the data in the HTML document
+      data.forEach((item) => {
+        const row = document.createElement('tr');
+        const timeCell = document.createElement('td');
+        const mq2Cell = document.createElement('td');
+        const dht22Cell = document.createElement('td');
+        const connectionCell = document.createElement('td');
+        timeCell.textContent = item.timeValue;
+        mq2Cell.textContent = item.mq2Value;
+        dht22Cell.textContent = item.dht22Value;
+        connectionCell.textContent = item.connectionValue;
+        row.appendChild(timeCell);
+        row.appendChild(mq2Cell);
+        row.appendChild(dht22Cell);
+        row.appendChild(connectionCell);
+        document.querySelector('.data-table tbody').appendChild(row);
+      });
+    };
+  };
+}
+
 
 timeout();
+displayData();
