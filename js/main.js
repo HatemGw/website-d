4e01798f73865e5ea8eab320b41761d3b62407d7
HatemGw <aassdd127zzee@gmail.com>
2023-06-17 04:43:57 +0200

    fasd

--- js/main.js
+++ js/main.js
@@ -18,38 +18,74 @@ timeout();
 }, 1000);
 }
 
-function getMq2() {
-fetch(
-"https://ny3.blynk.cloud/external/api/get?token=U-nfUdhCpO9A5VhMmw7c6nULbVJWnFvM&dataStreamId=6"
-).then((response) => {
-response.json().then((data) => {
-mq2Value.innerHTML = data;
-if (data == 0) {
-connectionMQ2 = 0;
+let fileWriter;
+let fileName = "data.txt";
+
+function initFileWriter() {
+  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+  window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function(fs) {
+    fs.root.getFile(fileName, {create: true}, function(fileEntry) {
+      fileEntry.createWriter(function(writer) {
+        fileWriter = writer;
+      });
+    });
+  });
 }
-});
-});
+
+function writeToFile(timeValue, mq2Value, dht22Value, connectionValue) {
+  if (fileWriter) {
+    const data = `${timeValue}\t${mq2Value}\t${dht22Value}\t${connectionValue}\n`;
+    const blob = new Blob([data], {type: "text/plain"});
+    fileWriter.write(blob);
+  }
+}
+
+function getMq2() {
+  fetch(
+    "https://ny3.blynk.cloud/external/api/get?token=U-nfUdhCpO9A5VhMmw7c6nULbVJWnFvM&dataStreamId=6"
+  ).then((response) => {
+    response.json().then((data) => {
+      mq2Value.innerHTML = data;
+      if (data == 0) {
+        connectionMQ2 = 0;
+      }
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      writeToFile(time, data, null, null);
+    });
+  });
 }
 
 function getDht22() {
-fetch(
-"https://ny3.blynk.cloud/external/api/get?token=U-nfUdhCpO9A5VhMmw7c6nULbVJWnFvM&dataStreamId=8"
-).then((response) => {
-response.json().then((data) => {
-dht22Value.innerHTML = data;
-if (data == 0) {
-connectionDHT22 = 0;
-}
-});
-});
+  fetch(
+    "https://ny3.blynk.cloud/external/api/get?token=U-nfUdhCpO9A5VhMmw7c6nULbVJWnFvM&dataStreamId=8"
+  ).then((response) => {
+    response.json().then((data) => {
+      dht22Value.innerHTML = data;
+      if (data == 0) {
+        connectionDHT22 = 0;
+      }
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      writeToFile(time, null, data, null);
+    });
+  });
 }
 
 function getConnection() {
-if (connectionMQ2 == 0 && connectionDHT22 == 0) {
-connectionValue.innerHTML = "offline";
-} else {
-connectionValue.innerHTML = "online";
-}
+  if (connectionMQ2 == 0 && connectionDHT22 == 0) {
+    connectionValue.innerHTML = "offline";
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    writeToFile(time, null, null, "offline");
+  } else {
+    connectionValue.innerHTML = "online";
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    writeToFile(time, null, null, "online");
+  }
 }
 
+initFileWriter();
+writeToFile();
 timeout();
